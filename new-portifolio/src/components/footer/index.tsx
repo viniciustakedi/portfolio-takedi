f2d61b68bf0cf18ec8a68e99be0cca3c59643905
viniciustakedi <viniciustakedi7@gmail.com>
2021-10-19 11:02:31 -0300

    terminar footer, alinhar no centro e responsividade

--- new-portifolio/src/components/footer/index.tsx
+++ new-portifolio/src/components/footer/index.tsx
@@ -1,11 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './footer-style.css';
 
 export default function Footer() {
     return(
         <div className="footer">
+            <hr />
+            <div className="content-informations-footer">
+                <div className="information">
+                    <h2>Contato -</h2>
+                    <span>Meu número e meu email estão disponíveis para contato.
+                        Eu prefiro o email para se comunicar, porém, utilize a melhor forma para você.
+                    </span>
+                    {/* <a href="mailto:viniciustakedi7@gmail.com">viniciustakedi7@gmail.com</a> */}
+                    <p>E: viniciustakedi7@gmail.com</p>
+                    <p>T: +55 11 95929-8849</p>
+                </div>
+                <div className="information">
+                <h2>Últimos projetos -</h2>
+                    <ul>
+                        <li><Link to="/">M2S Cars</Link></li>
+                        <li><Link to="/">D + S Seguros</Link></li>
+                        <li><Link to="/">DW Blindagens</Link></li>
+                    </ul>
+                </div>
+                <div className="information">
+                <h2>Disponibilidade -</h2>
+                    <span>Estou diponível para responder emails e mensagens principalmente em dias úteis,
+                        porém, sempre fico de olho nos meios de contatos mesmo em dias não úteis como sábado e domingo.
+                    </span>
+                </div>
+                <div className="information">
+                <h2>Social -</h2>
+                    <ul>
+                        <li><a href="https://www.linkedin.com/in/vinicius-takedi/" target="blank_">LinkedIn</a></li>
+                        <li><a href="https://github.com/viniciustakedi/" target="blank_">GitHub</a></li>
+                    </ul>
+                </div>
+            </div>
             <div className="content-footer">
-                <span>Portifólio desenvolvido por Vinicius Takedi 2021.</span>
+                <span>© Vinicius Takedi 2021. Made in São Paulo, Brazil.</span>
             </div>
         </div>
     );
