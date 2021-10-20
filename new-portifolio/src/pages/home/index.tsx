8bced083bb6448b679aa11059501f2bb5413e497
viniciustakedi <viniciustakedi7@gmail.com>
2021-10-20 16:03:57 -0300

    Home finalizar

--- new-portifolio/src/pages/home/index.tsx
+++ new-portifolio/src/pages/home/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Footer from "../../components/footer";
 import Menu from "../../components/menu";
+import { SiGithub, SiGmail, SiLinkedin } from "react-icons/si";
 import './home-style.css';
-import japanese from '../../assets/images/home-image.jpg';
+import FotoTakedi from '../../assets/images/foto-takedi.png';
 
 export default function Home() {
     return (
@@ -24,7 +25,14 @@ export default function Home() {
                     </div>
                     <div className="image-first-home">
                         <div className="opacity-background">
-
+                            <p>Entre em contato comigo!</p>
+                            <img src={FotoTakedi} alt="foto_takedi" />
+                            <div className="buttons-home">
+                                <a href="https://www.linkedin.com/in/vinicius-takedi/" target="_blank" rel="noopener noreferrer"><SiLinkedin id="icon-contact-home" color="#f3f3f3" size="2.5rem"/></a>
+                                <a href="https://github.com/viniciustakedi/" target="_blank" rel="noopener noreferrer"><SiGithub id="icon-contact-home" color="#f3f3f3" size="2.5rem"/></a>
+                                <a href="mailto:viniciustakedi7@gmail.com"><SiGmail id="icon-contact-home" color="#f3f3f3" size="2.5rem"/></a>
+                            </div>
+                            <span>Acesse minhas redes sociais ou email para comunicação.</span>
                         </div>
                     </div>
                 </section>
